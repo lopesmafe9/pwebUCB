cf464746fd46b65da966619cf98efffd691f578e
Your Name <you@example.com>
2025-02-28 20:45:31 -0300

    editing

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 10;
+
+soma = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+
+let soma = ('A soma de a + b é: ' + soma);
+let subtracao = ('A subtração de a - b é: ' + subtracao);
+let multiplicacao = ('A multiplicação de a * b é: ' + multiplicacao);
+let divisao = ('A divisão de a / b é: ' + divisao);
+
+console.log(soma);
+console.log(subtracao); 
+console.log(multiplicacao);
+console.log(divisao);
